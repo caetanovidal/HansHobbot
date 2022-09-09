563aa713270cf42deb347119f1a5ad561bf54dab
Caetano Menin Geteins Vidal <desenvolvimento2+caetano@injepro.com>
2022-09-09 15:48:17 -0300

    music player add

--- hans_hobbot_class.py
+++ hans_hobbot_class.py
@@ -1,81 +1,133 @@
 import discord
 import twitter_request
-from w2g_request import create_sala_w2g, update_room, add_to_playlist
+from w2g_request import create_sala_w2g, add_to_playlist
 import btc_request
-from helpers import get_twitter_user, get_quote, get_link_youtube
+from helpers import get_quote
+import discord
+from discord.ext import commands
+import asyncio
+import youtube_dl
 
-class Hans_hobbot_core(discord.Client):
-    async def on_ready(self):
-        print(f'Logged in as {self.user} (ID: {self.user.id})')
-        print('------')
+# Suppress noise about console usage from errors
+youtube_dl.utils.bug_reports_message = lambda: ''
+
+ytdl_format_options = {
+    'format': 'bestaudio/best',
+    'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
+    'restrictfilenames': True,
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': True,
+    'no_warnings': True,
+    'default_search': 'auto',
+    'source_address': '0.0.0.0',  # bind to ipv4 since ipv6 addresses cause issues sometimes
+}
+
+ffmpeg_options = {
+    'options': '-vn',
+}
+
+ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+
+class YTDLSource(discord.PCMVolumeTransformer):
+    def __init__(self, source, *, data, volume=0.5):
+        super().__init__(source, volume)
+
+        self.data = data
+
+        self.title = data.get('title')
+        self.url = data.get('url')
+
+    @classmethod
+    async def from_url(cls, url, *, loop=None, stream=False):
+        loop = loop or asyncio.get_event_loop()
+        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
+
+        if 'entries' in data:
+            # take first item from a playlist
+            data = data['entries'][0]
+
+        filename = data['url'] if stream else ytdl.prepare_filename(data)
+        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
     
-    async def on_message(self, message):
-        if message.author.id == self.user.id:
-            return
-        
-        if message.content.startswith('!hello'):
-            await self.hello(message)
-        
-        if message.content.startswith('!inspire'):
-            await self.inspire(message)
-        
-        if message.content.startswith('!get'):
-            await self.get(message)
-
-        if message.content.startswith('!w2g'):
-            await self.w2g(message)
+class Hans_robot(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
     
-        if message.content.startswith('!upw2g'):
-            update_room(message)
+    @commands.command()
+    async def get(self, ctx, message):
+        try:
+            last_id = twitter_request.get_last_tweet_id(message)
+            last_tweet_msg = twitter_request.get_last_tweet_msg(last_id)
+            await ctx.channel.send(last_tweet_msg)
+        except Exception as e:
+            print(e)
 
-        if message.content.startswith('!addw2g'):
-            msg = add_to_playlist(message)
-            await message.reply(msg)
+    @commands.command()
+    async def w2g(self, ctx, message):
+        try:
+            link_w2g = create_sala_w2g(message, ctx)
+            await ctx.reply(link_w2g)
+        except Exception as e:
+            print(e) 
 
-        if message.content.startswith('!btc'):
-            await self.btc(message)
-
-        if message.content.startswith("!help"):
-            msg = """
-            !get @twitterUser -> return the last tweet from that user
-!inspire -> get a inspire quote
-!w2g youtube/link -> create a room with the youtube link that you pass
-!btc -> send a ticket with the btc value and variation
-!upw2g -> must be done by the same person how create the room using !w2g, updates the room with a new video
-!addw2g -> must be done by the same person how create the room using !w2g, add a new video to de queue
-            """
-            try:
-                await message.channel.send(msg)
-            except Exception as e:
-                print(e)
+    @commands.command()
+    async def addw2g(self, ctx, message):
+        msg = add_to_playlist(message, ctx)
+        await ctx.reply(msg)
     
-    async def hello(self, message):
-        await message.reply("Hello motherfucker!")
-    
+    @commands.command()
+    async def btc(self, ctx):
+        try:
+            msg = btc_request.request_helper()
+            await ctx.reply(msg)
+        except Exception as e:
+            print(e)
+            
+    @commands.command()
+    async def hello(self, ctx):
+        await ctx.reply("Hello motherfucker!")
+
+    @commands.command()
     async def inspire(self, message):
         quote = get_quote()
         await message.reply(quote)
 
-    async def get(self, message):
-        user_name = get_twitter_user(message.content)
-        try:
-            last_id = twitter_request.get_last_tweet_id(user_name)
-            last_tweet_msg = twitter_request.get_last_tweet_msg(last_id)
-            await message.channel.send(last_tweet_msg)
-        except Exception as e:
-            print(e)
-               
-    async def w2g(self, message):
-        try:
-            link_w2g = create_sala_w2g(message)
-            await message.reply(link_w2g)
-        except Exception as e:
-            print(e) 
-    
-    async def btc(self, message):
-        try:
-            msg = btc_request.request_helper()
-            await message.reply(msg)
-        except Exception as e:
-            print(e) 
-    
+    @commands.command()
+    async def stream(self, ctx, *, url):
+        """Streams from a url (same as yt, but doesn't predownload)"""
+
+        async with ctx.typing():
+            player = await YTDLSource.from_url(url, loop=self.bot.loop, stream=True)
+            ctx.voice_client.play(player, after=lambda e: print(f'Player error: {e}') if e else None)
+
+        await ctx.send(f'Now playing: {player.title}')
+
+    @commands.command()
+    async def volume(self, ctx, volume: int):
+        """Changes the player's volume"""
+
+        if ctx.voice_client is None:
+            return await ctx.send("Not connected to a voice channel.")
+
+        ctx.voice_client.source.volume = volume / 100
+        await ctx.send(f"Changed volume to {volume}%")
+
+    @commands.command()
+    async def stop(self, ctx):
+        """Stops and disconnects the bot from voice"""
+
+        await ctx.voice_client.disconnect()
+
+    @stream.before_invoke
+    async def ensure_voice(self, ctx):
+        if ctx.voice_client is None:
+            if ctx.author.voice:
+                await ctx.author.voice.channel.connect()
+            else:
+                await ctx.send("You are not connected to a voice channel.")
+                raise commands.CommandError("Author not connected to a voice channel.")
+        elif ctx.voice_client.is_playing():
+            ctx.voice_client.stop()
--- w2g_request.py
+++ w2g_request.py
@@ -10,21 +10,21 @@ dict_user_streamkey = {}
 
 payloadHeader = {'Accept': 'application/json', 'Content-type': 'application/json'}
 
-def create_sala_w2g(message):
-    url = get_link_youtube(message.content)
+def create_sala_w2g(message, ctx):
+    url = message
     paylodJson = {"w2g_api_key": os.getenv('API_KEY_W2G'), 'share': url, 'bg_color': '#02000a', 'bg_opacity': '50'}
     r = requests.post("https://w2g.tv/rooms/create.json", headers=payloadHeader, json=paylodJson)
     
     stream_key = format_json_link(r.json())
     
-    user_id = message.author.id
+    user_id = ctx.author.id
     dict_user_streamkey[user_id] = stream_key
         
     return f"https://w2g.tv/rooms/{stream_key}"
 
 def update_room(message):
     try:
-        url_video = get_link_youtube(message.content)
+        url_video = message
         streak_key = dict_user_streamkey[message.author.id]
         url_post = f"https://w2g.tv/rooms/{streak_key}/sync_update"
         payloadJson = {"w2g_api_key": os.getenv('API_KEY_W2G'),
@@ -33,11 +33,11 @@ def update_room(message):
     except Exception as e:
         print(e)
 
-def add_to_playlist(message):
-    url_video = get_link_youtube(message.content)
-    title = message.embeds[0].title
+def add_to_playlist(message, ctx):
+    url_video = message
+    title = ctx.message.embeds[0].title
     try:
-        streak_key = dict_user_streamkey[message.author.id]
+        streak_key = dict_user_streamkey[ctx.author.id]
         url_post = f"https://w2g.tv/rooms/{streak_key}/playlists/current/playlist_items/sync_update"
         payloadJson = {"w2g_api_key": os.getenv('API_KEY_W2G'), "add_items": [{"url": url_video, "title": title}]}
         requests.post(url_post, headers=payloadHeader, json=payloadJson)
